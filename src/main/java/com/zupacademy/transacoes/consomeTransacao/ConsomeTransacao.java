e4addb15a98741728948b4305411ba113694c354
AlanaZUP <alana.reis@zup.com.br>
2021-10-07 10:14:58 -0300

    Recebendo tópicos 'transacoes' através do kafka

--- src/main/java/com/zupacademy/transacoes/consomeTransacao/ConsomeTransacao.java
+++ src/main/java/com/zupacademy/transacoes/consomeTransacao/ConsomeTransacao.java
@@ -1,2 +1,19 @@
-package com.zupacademy.transacoes.consomeTransacao;public class ConsomeTransacao {
+package com.zupacademy.transacoes.consomeTransacao;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsomeTransacao {
+
+    @KafkaListener(topics = "transacoes")
+    private void consome(ConsumerRecord<String, String> record){
+        System.out.println("-------------------------------");
+        System.out.println("Processing new order");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+    }
 }
