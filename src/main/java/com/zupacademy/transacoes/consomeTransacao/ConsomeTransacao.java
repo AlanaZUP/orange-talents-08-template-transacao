c3777cdee69aee8b6038643fb564e784a87a210d
AlanaZUP <alana.reis@zup.com.br>
2021-10-11 09:46:53 -0300

    Consumidor do mensageiro

--- src/main/java/com/zupacademy/transacoes/consomeTransacao/ConsomeTransacao.java
+++ src/main/java/com/zupacademy/transacoes/consomeTransacao/ConsomeTransacao.java
@@ -1,19 +1,49 @@
 package com.zupacademy.transacoes.consomeTransacao;
 
+import com.zupacademy.transacoes.cartao.Cartao;
+import com.zupacademy.transacoes.cartao.CartaoRepository;
+import com.zupacademy.transacoes.estabelecimento.Estabelecimento;
+import com.zupacademy.transacoes.estabelecimento.EstabelecimentoRepository;
+import com.zupacademy.transacoes.transacao.Transacao;
+import com.zupacademy.transacoes.transacao.TransacaoDtoForm;
+import com.zupacademy.transacoes.transacao.TransacaoRepository;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Component
 public class ConsomeTransacao {
 
-    @KafkaListener(topics = "transacoes")
-    private void consome(ConsumerRecord<String, String> record){
-        System.out.println("-------------------------------");
-        System.out.println("Processing new order");
-        System.out.println(record.key());
-        System.out.println(record.value());
-        System.out.println(record.partition());
-        System.out.println(record.offset());
+    @Autowired
+    private TransacaoRepository transacaoRepository;
+
+    @Autowired
+    private CartaoRepository cartaoRepository;
+
+    @Autowired
+    private EstabelecimentoRepository estabelecimentoRepository;
+
+    @KafkaListener(topics = "${spring.kafka.topic}")
+    @Transactional
+    private void consome(TransacaoDtoForm transacaoDtoForm) throws IllegalArgumentException {
+        transacaoRepository.findByIdOrigin(transacaoDtoForm.getId())
+                .ifPresent(transacao -> {
+                    throw new IllegalArgumentException("Já existe transação com o id " + transacaoDtoForm.getId());
+                });
+
+        Cartao cartao = transacaoDtoForm.getCartao().toModel();
+        cartao = cartaoRepository.findByIdOrigin(cartao.getIdOrigin()).orElse(cartaoRepository.save(cartao));
+
+        Estabelecimento estabelecimento = transacaoDtoForm.getEstabelecimento().toModel();
+        estabelecimento = estabelecimentoRepository.findByNomeAndCidadeAndEndereco(estabelecimento.getNome(), estabelecimento.getCidade(), estabelecimento.getEndereco()).orElse(estabelecimentoRepository.save(estabelecimento));
+
+        Transacao transacao = transacaoDtoForm.toModel(estabelecimento, cartao);
+
+        transacaoRepository.save(transacao);
     }
 }
