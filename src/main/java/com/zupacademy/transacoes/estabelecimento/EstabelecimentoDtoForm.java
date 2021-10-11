0a902b00d56f5599d48cde87e00f555f0d673512
AlanaZUP <alana.reis@zup.com.br>
2021-10-11 09:45:00 -0300

    Entity, Dto e Repository -> Estabelecimento

--- src/main/java/com/zupacademy/transacoes/estabelecimento/EstabelecimentoDtoForm.java
+++ src/main/java/com/zupacademy/transacoes/estabelecimento/EstabelecimentoDtoForm.java
@@ -1,2 +1,38 @@
-package com.zupacademy.transacoes.estabelecimento;public class EstabelecimentoDtoForm {
+package com.zupacademy.transacoes.estabelecimento;
+
+import javax.validation.constraints.NotBlank;
+
+public class EstabelecimentoDtoForm {
+    @NotBlank
+    private String nome;
+    @NotBlank
+    private String cidade;
+    @NotBlank
+    private String endereco;
+
+    @Deprecated
+    public EstabelecimentoDtoForm() {
+    }
+
+    public EstabelecimentoDtoForm(String nome, String cidade, String endereco) {
+        this.nome = nome;
+        this.cidade = cidade;
+        this.endereco = endereco;
+    }
+
+    public Estabelecimento toModel(){
+        return new Estabelecimento(nome, cidade, endereco);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
 }
--- src/main/java/com/zupacademy/transacoes/estabelecimento/EstabelecimentoRepository.java
+++ src/main/java/com/zupacademy/transacoes/estabelecimento/EstabelecimentoRepository.java
@@ -1,2 +1,10 @@
-package com.zupacademy.transacoes.estabelecimento;public interface EstabelecimentoRepository {
+package com.zupacademy.transacoes.estabelecimento;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento, Long> {
+
+    Optional<Estabelecimento> findByNomeAndCidadeAndEndereco(String nome, String cidade, String endereco);
 }
